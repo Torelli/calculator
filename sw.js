94bb3941a24a0c9db0a42e046fc4ca4548774147
Giovanni Torelli <26353442+Torelli@users.noreply.github.com>
2023-02-08 22:45:12 -0300

    New features for PWA

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/calculator';
 var APP_PREFIX = 't_calculator';
-var VERSION = 'version_009';
+var VERSION = 'version_010';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
